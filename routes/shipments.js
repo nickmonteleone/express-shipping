d727e295a6b84192a8a81105bc36e89bd01d043f
Victor Kim <vskimjr@gmail.com>
2024-01-09 21:04:20 -0500

    started on further study, added multipleShipmentsSchema, started route for post /multi

--- routes/shipments.js
+++ routes/shipments.js
@@ -4,6 +4,7 @@ const express = require("express");
 const { BadRequestError } = require("../expressError");
 const jsonschema = require("jsonschema");
 const shipmentSchema = require("../schemas/shipmentSchema.json")
+const multipleShipmentsSchema = require("../schemas/multipleShipmentsSchema.json")
 const router = new express.Router();
 
 const { shipProduct } = require("../shipItApi");
@@ -33,6 +34,40 @@ router.post("/", async function (req, res, next) {
   const { productId, name, addr, zip } = req.body;
   const shipId = await shipProduct({ productId, name, addr, zip });
   return res.json({ shipped: shipId });
+});
+
+/** POST /multi
+ *
+ * {productIds, name, addr, zip}
+ *
+ * returns JSON: { shipped: [shipId, shipId]] }
+ * where the shipId comes from each of ShipIt’s API response.
+ */
+
+router.post("/multi", async function (req, res, next) {
+  console.log('req body', req.body);
+  const validationResult = jsonschema.validate(
+    req.body,
+    multipleShipmentsSchema,
+    {required: true}
+  );
+  console.log('validationResult', validationResult);
+
+  if (!validationResult.valid) {
+    const errs = validationResult.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
+
+  const name = req.body.name;
+  const addr = req.body.addr;
+  const zip = req.body.zip;
+
+  const requestBodies = req.body.productIds.map(productId =>  {
+    return shipProduct({ productId, name, addr, zip })
+  });
+
+  return res.json({shipped: await promise.all(requestBodies)});
+
 });
 
 
